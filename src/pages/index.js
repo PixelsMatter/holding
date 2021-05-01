e936cf38a19c930b2766a0fc35c17bfc5340d035
Leon Burman シ <pixelsmatter1@gmail.com>
2021-05-01 16:39:08 +0100

    Update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,9 @@ const IndexPage = () => (
 		<SEO/>
 		<Container>
 			<Logo/>
-			<p>This is a fresh instance of the PixelsMatter Gatsby starter which includes Preact, SCSS using a variant
-				of 7-1 architecture, BEM, perfect Lighthouse scores, offline support, Google Analytics,
-				sitemaps &amp; plenty more good stuff!</p>
-			<a className='btn' href='https://github.com/PixelsMatter/gatsby-template' target='_blank' rel='noreferrer'>GitHub
-				Repo</a>
-			<a className='btn btn--alt' href='https://pixelsMatter.io' target='_blank' rel='noreferrer'>Website</a>
+			<p>Stay tuned for the launch of our new site! In the meantime, feel free to get in touch using hello@pixelsmatter.io. You can also join our newsletter and be the first to know when we go live.</p>
+			<a className='btn' href='mailto:hello@pixelsmatter.io' target='_blank' rel='noreferrer'>Say Hello</a>
+			<a className='btn btn--alt' href='https://eepurl.com/hxplL5' target='_blank' rel='noreferrer'>Keep Updated</a>
 		</Container>
 	</Layout>
 )
