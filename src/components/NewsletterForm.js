e2e3670ab10451247ed4e0ff54a6480b2f088207
Leon Burman シ <pixelsmatter1@gmail.com>
2021-05-26 14:01:24 +0100

    Feed form into Mailchimp API

--- src/components/NewsletterForm.js
+++ src/components/NewsletterForm.js
@@ -1,7 +1,7 @@
 // Libraries
 import React from 'react'
 import {Link} from 'gatsby'
-import Axios from 'axios'
+import AddToMailchimp from 'gatsby-plugin-mailchimp'
 
 // Images
 import backPath from '../img/back.svg'
@@ -13,6 +13,9 @@ class NewsletterForm extends React.Component {
 		this.state = {
 			firstName: null,
 			email: null,
+			success: false,
+			loading: false,
+			error: null,
 		}
 
 		this.handleChange = this.handleChange.bind(this)
@@ -33,18 +36,54 @@ class NewsletterForm extends React.Component {
 
 	handleSubmit(event) {
 		event.preventDefault()
+
+		this.setState({
+			loading : true,
+			error: null,
+		})
+
+		AddToMailchimp(this.state.email, {
+			FNAME: this.state.firstName,
+		}).then(response => {
+			this.setState({'loading' : false})
+
+			if(response.result === 'success') {
+				this.setState({'success' : true})
+			}
+			else {
+				this.setState({error: true})
+			}
+		})
 	}
 
 	render() {
 		return (
 			<form onSubmit={this.handleSubmit} className='form'>
-				<h1 className='form__heading'>Join Our Newsletter</h1>
-				<p className='form__text'>By submitting your details you agree to receive communication and newsletter updates from PixelsMatter. We never spam and you can unsubscribe at any time.</p>
-				<input type='text' name='firstName' required placeholder='First Name' onChange={this.handleChange} className='input'/>
-				<input type='email' name='email' required placeholder='Email' onChange={this.handleChange} className='input'/>
+				{!this.state.success ? (
+					<>
+						<h1 className='form__heading'>Join Our Newsletter</h1>
+						<p className='form__text'>By submitting your details you agree to receive communication and newsletter updates from PixelsMatter. We never spam and you can unsubscribe anytime.</p>
+						<input type='text' name='firstName' required placeholder='First Name' onChange={this.handleChange} disabled={this.state.loading} className='input'/>
+						<input type='email' name='email' required placeholder='Email' onChange={this.handleChange} disabled={this.state.loading} className='input'/>
+					</>
+				) :
+					<>
+						<h1 className='form__heading'>Thanks For Joining</h1>
+						<p className='form__text'>Your email address <strong>({this.state.email})</strong> has been added to the PixelsMatter mailing list. Look forward to plenty of helpful updates from us in the near future.</p>
+					</>
+				}
+
+				{this.state.error ? (
+					<div className='error'>Error, please try again later.</div>
+				) : null}
+
 				<div className='flex space-between full-width'>
 					<Link to='/' className='btn btn--subtle'><img width='25' height='25' src={backPath} alt='Arrow' className='btn__icon'/> Go Back</Link>
-					<button type='submit' name='submit' value='Submit' className='btn'>Submit</button>
+					{!this.state.success ? (
+						<button type='submit' name='submit' value='Submit' disabled={this.state.loading} className='btn'>{this.state.loading ? 'Loading...' : 'Submit'}</button>
+					) :
+						<a className='btn' href='mailto:hello@pixelsmatter.io' target='_blank' rel='noreferrer'>Say Hello</a>
+					}
 				</div>
 			</form>
 		)
