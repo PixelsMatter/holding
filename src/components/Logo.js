e663cd00e59cc7c86b982eb512f017db62558927
Leon Burman シ <pixelsmatter1@gmail.com>
2021-07-23 22:33:43 +0100

    Update logo image file name

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 // Images
-import logoPath from '../img/pixelsmatter-logo.svg'
+import logoPath from '../img/logo.svg'
 
 /*
 * gatsby image is not required here as SVG is vector based 
